9bc95c28df23a418df334d95fb5c9f738f5b6c71
Bryan Goss <bcgoss@gmail.com>
2016-11-02 11:33:48 -0600

    Refactoring Request

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,4 +1,16 @@
 class BestBuyService
   def self.stores_by_postal_code(postal_code)
+    BestBuyService.new(postal_code)
+  end
+
+  def initialize(postal_code)
+    @postal_code = postal_code
+  end
+
+  def conn 
+    Farday.new(url: 'https://api.bestbuy.com/v1') do |faraday|
+      faraday.request :url_encoded
+      faraday.adapter Faraday.default_adapter
+    end
   end
 end
