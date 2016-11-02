06c7e2eae642027a071175c608b531857196dd77
Bryan Goss <bcgoss@gmail.com>
2016-11-02 12:04:39 -0600

    Set up store to link to page displaying individual hours

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,10 +1,11 @@
 class Store
-  attr_reader :name, :city, :distance, :phone_number, :type_of_store
+  attr_reader :name, :city, :distance, :phone_number, :type_of_store, :store_id
   def initialize(raw_store)
     @name = raw_store[:name]
     @city = raw_store[:city]
     @distance = raw_store[:distance]
     @phone_number = raw_store[:phone]
     @type_of_store = raw_store[:storeType]
+    @store_id = raw_store[:storeId]
   end
 end
